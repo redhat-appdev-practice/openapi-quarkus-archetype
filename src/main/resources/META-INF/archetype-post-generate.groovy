b46f7877fb35048801411026b99d5003eb28c344
Deven Phillips <deven.phillips@redhat.com>
2021-07-04 11:22:57 -0400

    Set executable bit for maven wrapper on POSIX systems

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,20 @@
+/* groovylint-disable CompileStatic */
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.PosixFilePermissions
+
+
+String operatingSystem = System.getProperty("os.name").toLowerCase(Locale.ENGLISH)
+
+
+if (!operatingSystem.contains("windows")) {
+  Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
+
+  Properties properties = request.properties
+
+  Path mvnWrapper = Paths.get(projectPath, "mvnw")
+
+  Files.setPosixFilePermissions(mvnWrapper, PosixFilePermissions.fromString("rwxrwxrwx"))
+
+}
